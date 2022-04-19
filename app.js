fb9a78797914813ce4b290c86b174f9f6feef3fd
nora kramer <nora.kramer2@hva.nl>
2022-04-19 14:22:19 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+https://socket.io/get-started/chat
+*/
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(http)
+const port = process.env.PORT || 4000
+
+app.use(express.static(path.resolve('public')))
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('message', (message) => {
+    io.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+http.listen(port, () => {
+  console.log('listening on port ', port)
+})
