8195d14dff1ff7b51cfc1a043ca65e19c4cd3734
nora kramer <nora.kramer2@hva.nl>
2022-04-22 10:33:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
+
+
+require('dotenv').config()
+
 const port = process.env.PORT || 4000
 
 app.use(express.static(path.resolve('public')))
