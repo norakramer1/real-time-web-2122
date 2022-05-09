b9b1466c469d6fcd8520f0709dc291b1a30fc16b
nora kramer <nora.kramer2@hva.nl>
2022-05-09 12:24:44 +0200

    shows ur top songs!

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const SpotifyWebApi = require('spotify-web-api-node')
 
 require('dotenv').config()
 
-const port = process.env.PORT || 1234
+const port = process.env.PORT || 4000
 
 
 app.set('view engine', 'ejs');
@@ -19,7 +19,7 @@ app.set('views', './views');
 app.use(express.static(path.resolve('public')))
 
 
-// spotify api
+// spotify api scopes for retrieving information about artists, users
 const scopes = [
   'ugc-image-upload',
   'user-read-playback-state',
@@ -42,12 +42,14 @@ const scopes = [
   'user-follow-modify'
 ];
 
+// gets home route
 app.get('/', (req, res) => {
   res.render('home', {
     pageTitle: "Chat App Home"
   });
 });
 
+// sets a const for the authorization of user info
 const spotifyApi = new SpotifyWebApi({
   redirectUri: process.env.redirectUri,
   clientId: process.env.clientId,
@@ -59,6 +61,7 @@ app.get('/login', (req, res) => {
   
 });
 
+// if something goes wrong spotify redirects to localhost:4000/callback and logs any errors
 app.get('/callback', (req, res) => {
   const error = req.query.error;
   const code = req.query.code;
@@ -89,20 +92,22 @@ app.get('/callback', (req, res) => {
       res.send('Success! You can now close the window.');
 
 
-      setInterval(async () => {
-        const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body['access_token'];
-
-        console.log('The access token has been refreshed!');
-        console.log('access_token:', access_token);
-        spotifyApi.setAccessToken(access_token);
-      }, expires_in / 2 * 1000);
+      spotifyApi.refreshAccessToken().then(
+        function(data) {
+          console.log('The access token has been refreshed!');
+      
+          // Save the access token so that it's used in future calls
+          spotifyApi.setAccessToken(data.body['access_token']);
+        },
+        function(err) {
+          console.log('Could not refresh access token', err);
+        }
+      );
     })
     .catch(error => {
       console.error('Error getting Tokens:', error);
       res.send(`Error getting Tokens: ${error}`);
     });
-    res.redirect('/albums');
 });
 
 app.get('/albums', (req, res) => {
@@ -110,17 +115,32 @@ app.get('/albums', (req, res) => {
   spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
 
   //GET MY PROFILE DATA
-  function getMyData() {
-    (async () => {
-      const me = await spotifyApi.getMe();
-      console.log(me);
-    })().catch(e => {
-      console.error(e);
-    });
+  spotifyApi.getMyTopTracks()
+  .then(function(data) {
+    let topTracks = data.body.items;
 
+let songs = topTracks.map(element => {
+  return {
+    name: element.name,
+    album: element.album.name
   }
-  getMyData();
-  //  res.redirect
+})
+res.render('toptracks', {
+  pageTitle: 'my tracks',
+  songs: songs,
+});
+//console.log(topTracks);
+console.log(songs);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+
+  })
+
+  });
+
+app.get('/toptracks', (req, res) => {
+  console.log('topTracks here');
+  
 });
 
 //sockets
@@ -135,6 +155,8 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 })
+
+
 
 http.listen(port, () => {
   console.log('listening on port ', port)
